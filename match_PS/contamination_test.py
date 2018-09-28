934593462821eb999e3d27fa2da2879d69d7f1d8
DZuercher <dominikz@student.ethz.ch>
2018-09-28 21:22:26 +0900

    Corrected contamination testing

--- match_PS/contamination_test.py
+++ match_PS/contamination_test.py
@@ -14,7 +14,7 @@ def NFW(r, rho0, rs):
 def einasto(r, rho0, A, alpha):
     return rho0*np.exp(-A*r**alpha)
 
-contamination = 0.2
+contamination = 0.5
 rsteps = 25
 radmax = 10.0
 
@@ -33,7 +33,7 @@ red_par_array = np.asarray([rhos, alpha, rs, rho0, se, rt, beta, gamma])
 
 
 input_dir = "/work/dominik.zuercher/Output/splashpipe"
-type_ = "Planck_PS_21.5_blue_cut_7"
+type_ = "Planck_PS_21.5_blue_hard_spline"
 data = pd.read_csv("%s/%s/xi_2d.dat" % (input_dir, type_), header=None, sep = ' ')
 cov_data = pd.read_csv("%s/%s/xi_2d_cov.dat" % (input_dir, type_), header=None, sep = ' ')
 
@@ -51,44 +51,51 @@ rrange=np.linspace(rr[0],rr[-1],rsteps)
 #weights = np.asarray([1.0 - 1.0/25.0*n for n in range(25)])
 
 
-blue_corr = np.genfromtxt("/work/dominik.zuercher/Output/Mest/Planck_PS_21.5_blue_cut_7/plotsave.dat")
-blue_corr = blue_corr[rsteps:rsteps*2,0]
+blue_array = np.genfromtxt("/work/dominik.zuercher/Output/Mest/Planck_PS_21.5_blue_hard_spline_no_mc/plotsave.dat")
+blue_corr = blue_array[rsteps:rsteps*2,0]
+blue_true_dev = blue_array[rsteps*3:rsteps*4,0]
 #popt, pcov = curve_fit(einasto, rrange, blue_corr)
 #blue_rho = einasto(rrange, 10.0, 0.5, 4.0)
 #blue_dev = np.gradient(blue_rho, rrange)
 
 
-p = np.polyfit(rrange, np.log(blue_corr), deg=8)
+#p = np.polyfit(rrange, np.log(blue_corr), deg=8)
 
-blue_rho = np.poly1d(p)
+#blue_rho = np.poly1d(p)
 #blue_dev = blue_rho.deriv()
+
+blue_rho = rrange**(-1.5)
+blue_dev = rrange*(-1.5*rrange**(-2.5))/(rrange**(-1.5))
+
 
 red_rho = tools.einasto_profile(rrange,red_par_array)
 red_dev = tools.logdev_einasto_profile(rrange,red_par_array)
-blue_dev_proto = np.gradient(np.exp(blue_rho(rrange)),rrange)
-blue_dev = rrange/np.exp(blue_rho(rrange))*blue_dev_proto
+#blue_dev_proto = np.gradient(np.exp(blue_rho(rrange)),rrange)
+#blue_dev = rrange/np.exp(blue_rho(rrange))*blue_dev_proto
 
+full = contamination*red_rho + (1.0 - contamination)*blue_rho
+full_dev = contamination*red_dev + (1.0 - contamination)*blue_dev
 
-full = contamination*red_rho + (1.0 - contamination)*np.exp(blue_rho(rrange))
-full_dev = rrange*np.gradient(full, rrange)/full
 
 
 f, ax = plt.subplots(ncols = 2, nrows = 1)
-ax[0].plot(rrange,red_rho,'r-')
-ax[0].plot(rrange,full,'k-')
-ax[0].plot(rrange,np.exp(blue_rho(rrange)),'b-')
-ax[0].plot(rrange, blue_corr)
-ax[1].plot(rrange,red_dev,'r-')
-ax[1].plot(rrange[:20], blue_dev[:20],'b-')
-ax[1].plot(rrange[:20], full_dev[:20],'k-')
+ax[0].plot(rrange, red_rho,'r-', label = r"Red 21.5 (6 $\sigma$)")
+ax[0].plot(rrange, full,'k-', label = "50\% contamination by Reds")
+ax[0].plot(rrange, blue_rho,'b-', label = r"$r^{-1.5}$ power law")
+ax[0].plot(rrange, blue_corr,'b--', label = r"Blue 21.5 (6 $\sigma$)")
+ax[1].plot(rrange, red_dev,'r-')
+ax[1].plot(rrange, blue_dev,'b-')
+ax[1].plot(rrange, full_dev,'k-')
+ax[1].plot(rrange, blue_true_dev,'b--')
 ax[0].set_xscale('log')
 ax[1].set_xscale('log')
 #ax[0].set_ylim([0,2])
 ax[0].set_yscale('log')
-ax[1].set_xlim([1,6])
+#ax[1].set_xlim([1,6])
 ax[0].set_xlabel(r"$r$ (h$^{-1}$Mpc)")
 ax[1].set_xlabel(r"$r$ (h$^{-1}$Mpc)")
 ax[0].set_ylabel(r"$\xi_{\rm 3D}(r)$")
+f.legend()
 f.savefig("/work/dominik.zuercher/Output/match_PS/add_corr.pdf")
 
 
